991527b2b5aebae6eb1da141c0376a05a647ad5a
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-08-12 23:46:11 -0300

    add section main

--- src/components/landing/destaque/Slogan.tsx
+++ src/components/landing/destaque/Slogan.tsx
@@ -1,3 +1,12 @@
 export default function Slogan() {
+
+    return (
+        <div className={`
+        flex flex-col items-center md:items-sta
+        text-white font-light
+    `}>
+
+    </div>
+    )
     
 }
--- src/components/landing/destaque/index.tsx
+++ src/components/landing/destaque/index.tsx
@@ -1,9 +1,10 @@
 import Area from "../comum/Area";
+import Slogan from "./Slogan";
 
 export default function Destaque() {
     return (
-        <Area id="inicio">
-           
+        <Area id="inicio" className="pt-20">
+           <Slogan />
         </Area>
     )
 }
