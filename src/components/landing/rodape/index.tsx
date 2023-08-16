d62dc2a3223a7e8f696420179515d27296b1311c
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-08-15 22:57:02 -0300

    add footer

--- src/components/landing/rodape/index.tsx
+++ src/components/landing/rodape/index.tsx
@@ -1,10 +1,23 @@
 import Area from "../comum/Area";
 import Logo from "../comum/Logo";
+import RedesSociais from "./RedeSociais";
 
 export default function Rodape() {
     return (
-        <Area>
-            
+        <Area className="bg-black py-20">
+            <div className="flex flex-col items-center md:items-start ">
+                <Logo />
+                <div className="mt-3 text-zinc-400 text-center md:text-left">
+                    <div>Controle financeiro</div>
+                    <div className="flex gap-1.5">que <span className="font-black text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-cyan-600">facilita</span> sua vida</div>
+                </div>
+            </div>
+            <div className="flex flex-col md:flex-row md:justify-between items-center mt-14">
+                <RedesSociais />
+                <p className="text-zinc-100 mt-7 md:mt-0 text-center">
+                    <span className="font-bold">Aristófanes<span className="text-red-500"> Lira</span></span> ® {new Date().getFullYear()} - Todos os direitos reservados
+                </p>
+            </div>
         </Area>
     )
 }
